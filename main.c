13d6197e508ab6ba5384a0d48467a80e17ee30d3
allenkun <allen_kung@163.com>
2022-10-06 02:01:42 +0800

    位于分支 main 您的分支与上游分支 'origin/main' 一致。 要提交的变更： 	修改：     main.c 	新文件：   plantuml_test.md 	新文件：   plantuml_test.puml

--- main.c
+++ main.c
@@ -1,7 +1,70 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool IsPrime(int num)
+{
+	for (int i = 2; i * i <= num; i++)
+	{
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+int CountPrimeBF(int num)
+{
+	int count = 0;
+	for (int i = 2; i < num; i++)
+	{
+		if (IsPrime(i)) {
+			count = count + 1;
+		}
+	}
+	
+	return count;
+}
+
+int CountPrime(int n)
+{
+	int *map = (int *)malloc(sizeof(int) * n);
+	if (map == NULL) {
+		return 0;
+	}
+	// 假设所有数都是素数， 1 表示素数 0 表示合数
+	memset(map, 1, sizeof(int) * n);
+	map[0] = 0;
+	map[1] = 0;
+
+	for (int i = 2; i < n; i++)
+	{
+		if (map[i] == 0 && IsPrime(i)) {
+
+		}
+		
+	}
+	
+}
 
 int main()
 {
-	printf("Hello");
+	printf("Hello\n");
+
+	int res = CountPrime(2);
+	printf("%d\n\n", res);
+
+	res = CountPrime(3);
+	printf("%d\n\n", res);
+
+	res = CountPrime(4);
+	printf("%d\n\n", res);
+	res = CountPrime(6);
+	printf("%d\n\n", res);
+	res = CountPrime(7);
+	printf("%d\n\n", res);
+
+	res = CountPrime(100);
+	printf("%d\n\n", res);
 }
 
---
+++ plantuml_test.md
@@ -0,0 +1,11 @@
+# plantuml test
+## Activity Diagram
+```plantuml
+@startuml
+Alice -> Bob : Hello 
+Bob -> Alice : 你好
+
+@enduml
+```
+
+## Sequence Diagram
